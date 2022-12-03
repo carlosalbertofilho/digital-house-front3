b4dfb3fd17c91c9285c938b1ab65b46b60ada57a
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-12-03 09:54:43 -0300

    Mesa Aula 13

--- src/aulas/aula13/index.jsx
+++ src/aulas/aula13/index.jsx
@@ -1,25 +1,29 @@
-import { useEffect, useState } from 'react'
-import './style.scss'
+import { useEffect, useState } from "react";
+import "./style.scss";
 
 export function Aula13() {
+  const [pedido, setPedido] = useState(false);
 
-  const [contador, setContador] = useState(0)
-
-  const adicionarContador = () => {
-    setContador(contador + 1)
+  const mostraPedido = () => {
+    setPedido(!pedido);
   };
 
-  useEffect( () => {
-    console.log("Carregou")
-  }, [])
-
+  useEffect(() => {
+    const time = setTimeout(() => {
+      console.log("Espera 2 segundos");
+      mostraPedido();
+    }, 2000);
+    return () => {
+      clearTimeout(time);
+    };
+  }, []);
 
   return (
-    <main className='aula13-component'>
-      <h1 className='title-wrapper'> Contador </h1>
-      <button onClick={ adicionarContador }>Adicionar</button>
-      <span>Número Atual: { contador }</span>
-
+    <main className="aula13-component">
+      <h1 className="title-wrapper">
+        Seu Pedido: {pedido ? "Pizza" : ""}
+      </h1>
+      <button onClick={mostraPedido}>Cancelar Pedido</button>
     </main>
   );
 }
